9fbab0caf0673791e57bbe51c56d3ca396be59cc
priyks <priyankakulkarni3431@gmail.com>
2021-03-04 16:43:41 +0530

    added image in database using lob Entity

--- HibernatePractice/src/main/java/com/hibernate/App.java
+++ HibernatePractice/src/main/java/com/hibernate/App.java
@@ -8,11 +8,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws IOException
